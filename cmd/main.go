26cca7b51297fe3d1aed03726a10f42353470fd5
Denis Kalbasov <kds-ru@list.ru>
2025-07-23 11:28:02 +1000

    logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/DeneesK/sub-service/internal/config"
+import (
+	"github.com/DeneesK/sub-service/internal/config"
+	"github.com/DeneesK/sub-service/pkg/logger"
+)
 
 func main() {
 	conf := config.MustLoad()
+
+	log := logger.NewLogger(conf.LogLevel)
 
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,29 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+)
+
+const (
+	debug = "debug"
+	info  = "info"
+)
+
+func NewLogger(logLevel string) *zap.SugaredLogger {
+	var logger *zap.Logger
+	var err error
+
+	switch logLevel {
+	case debug:
+		logger, err = zap.NewDevelopment()
+	case info:
+		logger, err = zap.NewProduction()
+	}
+
+	if err != nil {
+		logger.Fatal("failed to initialized new logger", zap.String("err", err.Error()))
+	}
+
+	sugar := logger.Sugar()
+	return sugar
+}
